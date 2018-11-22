cc99cbb604e4fffd4dd43e9d3bf1213b382589d2
qinpeng <791360277@qq.com>
2018-11-22 18:00:59 +0800

    access token modify

--- src/main/java/com/sezioo/wechar_demo/service/WechatBottonService.java
+++ src/main/java/com/sezioo/wechar_demo/service/WechatBottonService.java
@@ -1,11 +1,17 @@
 package com.sezioo.wechar_demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sezioo.wechar_demo.property.WechatProperty;
 
 @Service
 public class WechatBottonService {
 	
 	private static String URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
+	
+	@Autowired
+	private WechatProperty wechatProperty;
 	
 	public String flushMenu() {
 		
