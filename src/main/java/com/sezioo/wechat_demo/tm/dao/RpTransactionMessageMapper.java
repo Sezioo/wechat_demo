52a95a126ecf559a33401aed7bc404d5feee0ae8
qinpeng <791360277@qq.com>
2019-02-15 17:30:05 +0800

    add mybatis generator

---
+++ src/main/java/com/sezioo/wechat_demo/tm/dao/RpTransactionMessageMapper.java
@@ -0,0 +1,7 @@
+package com.sezioo.wechat_demo.tm.dao;
+
+import com.sezioo.wechat_demo.tm.entity.RpTransactionMessage;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface RpTransactionMessageMapper extends Mapper<RpTransactionMessage> {
+}
